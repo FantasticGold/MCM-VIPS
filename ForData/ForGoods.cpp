cd8ea8e9d3857317c0701b71ec1d00d2997eb9d3
FantasticGold <FantasticGold@users.noreply.github.com>
2017-07-12 12:06:04 +0800

    new data
    sort and count

---
+++ ForData/ForGoods.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+using std::cout;
+using std::endl;
+using std::string;
+#include <map>
+#include <utility>
+
+int main(void) {
+  std::ofstream ofstm;
+  std::ifstream ifstm;
+
+  //Initialize
+  ifstm.open("../../Data/goods_train.txt");
+  std::map<string, std::pair<string, string> > m;
+  string spu_id, brand_id, cat_id;
+  while (ifstm) {
+    ifstm >> spu_id >> brand_id >> cat_id;
+    m[spu_id] = std::make_pair(brand_id, cat_id);
+  }
+  ifstm.close();
+
+  // // Sort
+  // ofstm.open("../NewData/goods_sort_data.txt");
+  // for (auto iter: m) {
+  //   ofstm << iter.first << " " << iter.second.first << " " << iter.second.second << endl;
+  // }
+  // ofstm.close();
+
+  // // Count
+  // ofstm.open("../NewData/goods_brand_data.txt");
+  // std::map<string, int> m2, m3;
+  // for (auto iter: m) {
+  //   ++m2[iter.second.first];
+  //   ++m3[iter.second.second];
+  // }
+  // for (auto iter: m2) {
+  //   ofstm << "Brand: " << iter.first << "   Goods: " << iter.second << endl;
+  // }
+  // ofstm.close();
+  // ofstm.open("../NewData/goods_category_data.txt");
+  // for (auto iter: m3) {
+  //   ofstm << "Category: " << iter.first << "   Goods: " << iter.second << endl;
+  // }
+  // ofstm.close();
+  
+  return 0;
+}
